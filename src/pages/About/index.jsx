4d7bcfbcc31772c9bda5ba5c207b213f59085f49
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-14 17:05:02 -0300

    setting about pages

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about">
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic odit
+        quisquam accusantium porro at necessitatibus obcaecati unde aliquam ipsa
+        laboriosam voluptatem rem laborum, vitae fuga debitis perferendis
+        corporis corrupti harum?
+      </p>
+      <Link to={"/posts/create"} className="btn">
+        Criar posts
+      </Link>
     </div>
   );
 }
