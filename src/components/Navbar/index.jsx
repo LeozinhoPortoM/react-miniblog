b5d108526924b1885c89c3edc8be547399b3c69e
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-13 22:49:22 -0300

    setting navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,23 @@
-import "./style.css"
+import "./style.css";
 
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../Logo";
 
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/about"}>About</NavLink>
+      <Link to={"/"}>
+        <Logo />
+      </Link>
+
+      <div className="search">
+        <input type="search" name="search" />
+      </div>
+
+      <div className="links">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+      </div>
     </nav>
   );
 }
