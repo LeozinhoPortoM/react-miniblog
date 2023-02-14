3d1f035a7d82bf0bafeeaa520874cb69695a3037
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-13 22:48:17 -0300

    creating footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <span>
+        Mini Blog desenvolvido com{" "}
+        <i className="fa fa-heart" style={{ color: "red" }}></i> por{" "}
+        <strong>Léo Porto Maranhão</strong>
+      </span>
+    </footer>
+  );
+}
