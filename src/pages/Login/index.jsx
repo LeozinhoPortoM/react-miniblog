c1f0c94800a7d388daec67937214371a4b530f97
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-14 22:49:42 -0300

    setting login page and login style

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,74 @@
-import "./style.css"
+import "./style.css";
 
-export default function Login(){
-    return (
-        <div>Login</div>
-    )
+import { useState, useEffect } from "react";
+
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { login, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = { email, password };
+
+    const res = await login(user);
+
+    console.log(res);
+
+    handleClear();
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  const handleClear = () => {
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="login">
+      <h1>Entrar</h1>
+      <p>Faça o login para utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            required
+            placeholder="E-mail do usuário"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            required
+            placeholder="Insira sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
 }
