4b08c2fb3b6dbb43afac2c2a3942be5cd2454124
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-16 22:36:13 -0300

    created postDetail component

---
+++ src/components/PostDetail/index.jsx
@@ -0,0 +1,24 @@
+import "./style.css";
+
+import { Link } from "react-router-dom";
+
+export default function PostDetail({ post }) {
+  return (
+    <div className="post_detail">
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p className="createdby">{post.createdBy}</p>
+      <div className="tags">
+        {post.tags.map((tag, i) => (
+          <p key={i}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler
+      </Link>
+    </div>
+  );
+}
