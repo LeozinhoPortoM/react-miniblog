0cc6cb758a1f79f57caf9a200c65d0981cc1791b
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-17 23:08:05 -0300

    adjusting postDetail components

--- src/components/PostDetail/index.jsx
+++ src/components/PostDetail/index.jsx
@@ -6,19 +6,21 @@ export default function PostDetail({ post }) {
   return (
     <div className="post_detail">
       <img src={post.image} alt={post.title} />
-      <h2>{post.title}</h2>
-      <p className="createdby">{post.createdBy}</p>
-      <div className="tags">
-        {post.tags.map((tag, i) => (
-          <p key={i}>
-            <span>#</span>
-            {tag}
-          </p>
-        ))}
+      <div className="post_detail__info">
+        <h2>{post.title}</h2>
+        <p className="createdby">{post.createdBy}</p>
+        <div className="tags">
+          {post.tags.map((tag, i) => (
+            <p key={i}>
+              <span>#</span>
+              {tag}
+            </p>
+          ))}
+        </div>
+        <Link to={`/posts/${post.id}`} className="btn btn-outline">
+          Ler
+        </Link>
       </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">
-        Ler
-      </Link>
     </div>
   );
 }
